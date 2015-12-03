116ca5cef2ae751893b51ecce0523e754a6a603a
imrokas <rokas@imrokas.com>
2015-12-02 23:45:46 -0500

    half implemented algorithm for finding all the rooks solutions

--- src/solvers.js
+++ src/solvers.js
@@ -14,18 +14,62 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution;
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  var recurse = function (n, board, row, col) {
+    if (n === 0) {
+      solution = board;
+      return true;
+    }
+    board.togglePiece(row, col);
+    if ( !board.hasAnyRooksConflicts() ) {
+      if ( recurse(n-1, board, row+1, col+1) ) return true;
+    }
+    board.togglePiece(row, col);
+    if ( recurse(n, board, row, col+1) ) return true;
+    if ( recurse(n, board, row+1, col) ) return true;
+    return false;
+  };
+
+  if (n === 1) solution = new Board([[1]]);
+  else {
+    recurse(n, new Board({n: n}), 0, 0);  
+  }
+
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution.rows()));
+  return solution.rows();
 };
 
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  
+  var isOutOfBounds = function (i) { return i >= n; };
 
+  var recurse = function (rookCount, board, c) {
+    if (rookCount === 0) {
+      _.each(board.rows(),function(r){console.log(r);});
+      solutionCount++;
+      return true;
+    } 
+    if (isOutOfBounds(c)) return false;
+    for (var row = 0; row < n; row++) {
+      board.togglePiece(row, c);
+      if (!board.hasAnyRooksConflicts()) {
+        if ( recurse(rookCount-1, board, c+1) ) {
+          board.togglePiece(row, c);
+        } else {
+          break;
+        } 
+      } else {
+        board.togglePiece(row, c);
+      }
+    }
+    return true;
+  };
+  recurse(n, new Board({n:n}), 0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
